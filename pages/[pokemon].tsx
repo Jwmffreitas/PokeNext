673cd95bbabcb2f1fc65a4c2fb1bbc555da34a2a
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-07-23 14:19:34 -0300

    Polishing pokemon data

--- pages/[pokemon].tsx
+++ pages/[pokemon].tsx
@@ -1,14 +1,68 @@
-import { getPokemonData } from './api/pokemon'
+import { getPokemonData, getPokemonLocation } from './api/pokemon'
 import Head from 'next/head'
+import styled from 'styled-components'
 
-function pokemonPage({ pokemonData }) {
+function pokemonPage({ pokemonData, pokemonLocation }) {
+    console.log(pokemonData)
+    console.log(pokemonLocation)
+
+    const Section = styled.section`
+    max-width: 40rem;
+    padding: 0 1rem;
+    margin: 3rem auto 3rem;
+    display: flex;
+    justify-content: space-evenly;
+  `
+
+    const PokemonImage = styled.img`
+    height: 280px
+    `
+
+    const PokemonName = styled.h1`
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 1.8rem;
+    color: black;
+    `
+
+    const Locations = styled.h2`
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 1.8rem;
+    color: black;
+    `
+
+    const LocationList = styled.ul`
+    list-style: none;
+    padding: 0px;
+    `
+
     return (
         <>
         <Head>
             <title>{pokemonData.name}</title>
         </Head>
 
-        <h1>{pokemonData.name}</h1>
+        <Section>
+            <PokemonImage src={pokemonData.sprites.other.dream_world.front_default}/>
+            <div>
+                <PokemonName>{pokemonData.name}</PokemonName>
+                <p>Types:</p>
+                <ul>
+                    {pokemonData.types.map((types: object) => (
+                        <li>{types.type.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </Section>
+        <Section style={{flexDirection: 'column', textAlign: 'center'}}>
+            <Locations>Encounters</Locations>
+            <LocationList>
+                {pokemonLocation.length != 0 ? pokemonLocation.map((location: object) => (
+                            <li>{location.location_area.name}</li>
+                )) : "No locations"} 
+            </LocationList>
+        </Section>
         </>
     )
 }
@@ -17,9 +71,11 @@ export async function getServerSideProps({params} : any) {
     // Fetch data from external API
     const res = await getPokemonData(params.pokemon)
     const pokemonData = res
+
+    const pokemonLocation = await getPokemonLocation(pokemonData.id)
   
     // Pass data to the page via props
-    return { props: { pokemonData } }
+    return { props: { pokemonData, pokemonLocation } }
 }
 
 export default pokemonPage
--- pages/api/pokemon.ts
+++ pages/api/pokemon.ts
@@ -1,16 +1,21 @@
 export async function getData() {
   // Instead of the file system,
   // fetch post data from an external API endpoint
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/')
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
   return res.json()
 }
 
 export async function getPokemonData(name) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-  const data = await res
+  const req = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  let res = await req
 
-  if(!data || data == undefined || data == null) return {notFound: true}
+  //if(!data || data == undefined || data == null) return {notFound: true}
   // Combine the data with the id
 
-  return data.json()
+  return res.json()
+}
+
+export async function getPokemonLocation(id: number) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/encounters`)
+  return res.json()
 }
